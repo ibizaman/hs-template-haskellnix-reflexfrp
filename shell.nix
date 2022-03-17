c5e9dd149e1dbbfe400eb42382a6005123f2d3d1
ibizaman <ibizapeanut@gmail.com>
2022-03-17 11:05:30 -0700

    frontend compilable with external ghcjs

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 (import ./default.nix).shellFor {
   tools = {
     cabal = "latest";
+    happy = "1.20.0"; # Needed for haskell-src-exts
     hlint = "latest";
     hspec-discover = "latest";
     haskell-language-server = "latest";
